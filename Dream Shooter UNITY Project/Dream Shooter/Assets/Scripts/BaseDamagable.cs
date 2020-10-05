c26018eb744fada9655e6ac7b9b15521b24e40e5
Daulton Nelson <60152571+DaultonNelson@users.noreply.github.com>
2020-10-05 12:26:07 -0400

    Simple Project Setup Stuff

---
+++ Dream Shooter UNITY Project/Dream Shooter/Assets/Scripts/BaseDamagable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseDamagable : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// The current health value of this Damagable.
+    /// </summary>
+    public int currHealthValue;
+    /// <summary>
+    /// The tag the colliding object needs to have to damage this Damagable
+    /// </summary>
+    public string damagingTag;
+    #endregion
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == damagingTag)
+        {
+            currHealthValue--;
+
+            DeathCheck();
+        }
+    }
+
+    private void DeathCheck()
+    {
+        if (currHealthValue <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Dream Shooter UNITY Project/Dream Shooter/Assets/Scripts/SimpleBullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleBullet : MonoBehaviour
+{
+    #region Variables
+    /// <summary>
+    /// The speed at which the bullet moves.
+    /// </summary>
+    public float moveSpeed;
+    //TODO: Implement this in BaseDamagable
+    /// <summary>
+    /// The amount this bullet damages a Damagable
+    /// </summary>
+    public int damageValue;
+    #endregion
+
+}
